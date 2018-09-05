7792ad46d2ed931608b4ab8e7e3071a858dafae8
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-09-05 02:28:53 -0400

    Add pipfile and rename pypi module to prvd

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="provide",
+    name="prvd",
     version="0.0.1",
     author="Kyle Thomas",
     author_email="kyle@provide.services",
