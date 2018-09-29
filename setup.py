3108eb774d4b3569b7e3c4fb4547b2edfad77133
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-09-29 03:28:06 -0400

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/provideservices/provide-python',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
     classifiers=[
     ],
 )
