f92382301d285a8e46d826e20ca04b0e2ddf9432
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-09-29 03:31:54 -0400

    Rev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='prvd',
-    version='0.0.1',
+    version='0.0.2',
     author='Kyle Thomas',
     author_email='kyle@provide.services',
     description='Provide python client',
